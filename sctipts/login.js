840cc109edea7ffb1545b678d1e14abe885bdd6b
Muhammad Ali <muhammadalik619@gmail.com>
2021-10-27 09:26:28 +0500

    initial commit

---
+++ sctipts/login.js
@@ -0,0 +1,37 @@
+'use strict'
+
+console.log("hello");
+
+
+let state = false;
+
+function toggle() {
+    const pass = document.getElementById("password");
+    // const pass1 = document.getElementById("password1");
+
+    if (state) {
+        pass.setAttribute("type", "password");
+        state = false;
+
+    } else {
+        pass.setAttribute("type", "text");
+        state = true;
+
+    }
+
+}
+
+function toggle1() {
+    const pass1 = document.getElementById("password1");
+
+    if (state) {
+        pass1.setAttribute("type", "password");
+        state = false;
+
+    } else {
+        pass1.setAttribute("type", "text");
+        state = true;
+
+    }
+
+}
